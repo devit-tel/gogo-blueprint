6012c87356ffae888fb5534218ebd2c9919a8e81
nqmt <to.nqmt@gmail.com>
2019-12-04 11:41:18 +0700

    update jaeger setup

--- setup.go
+++ setup.go
@@ -5,34 +5,43 @@ import (
 	"log"
 
 	logrustash "github.com/bshuster-repo/logrus-logstash-hook"
+	"github.com/devit-tel/goxid"
+	"github.com/opentracing/opentracing-go"
+	"github.com/sirupsen/logrus"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	jaegerlog "github.com/uber/jaeger-client-go/log"
+	"github.com/uber/jaeger-lib/metrics"
+
 	"github.com/devit-tel/gogo-blueprint/app"
 	"github.com/devit-tel/gogo-blueprint/config"
 	companyRepo "github.com/devit-tel/gogo-blueprint/repository/company/store"
 	staffRepo "github.com/devit-tel/gogo-blueprint/repository/staff/store"
 	companyService "github.com/devit-tel/gogo-blueprint/service/company"
 	staffService "github.com/devit-tel/gogo-blueprint/service/staff"
-	"github.com/devit-tel/goxid"
-	"github.com/opentracing/opentracing-go"
-	"github.com/sirupsen/logrus"
-	"github.com/uber/jaeger-client-go"
-	"github.com/uber/jaeger-client-go/transport/zipkin"
 )
 
-func setupJaeger(appConfig *config.Config) (opentracing.Tracer, io.Closer) {
-	transport, err := zipkin.NewHTTPTransport(
-		appConfig.JaegerEndpoint,
-		zipkin.HTTPBatchSize(50),
-		zipkin.HTTPLogger(jaeger.StdLogger),
+func setupJaeger(appConfig *config.Config) io.Closer {
+	cfg, err := jaegercfg.FromEnv()
+	if err != nil {
+		log.Panic(err)
+	}
+	cfg.ServiceName = appConfig.AppName
+	cfg.Sampler.Type = "const"
+	cfg.Sampler.Param = 1
+
+	jLogger := jaegerlog.StdLogger
+	jMetricsFactory := metrics.NullFactory
+
+	tracer, closer, err := cfg.NewTracer(
+		jaegercfg.Logger(jLogger),
+		jaegercfg.Metrics(jMetricsFactory),
 	)
 	if err != nil {
-		log.Fatalf("Cannot initialize HTTP transport: %v", err)
+		log.Panic(err)
 	}
+	opentracing.SetGlobalTracer(tracer)
 
-	return jaeger.NewTracer(
-		"GoGoBoilerplate",
-		jaeger.NewConstSampler(true),
-		jaeger.NewRemoteReporter(transport),
-	)
+	return closer
 }
 
 func newApp(appConfig *config.Config) *app.App {
